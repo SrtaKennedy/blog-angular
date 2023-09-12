8371fec5b8cc3ee411321aa385697a64d47c394d
SrtaKennedy <ruthellen1800@gmail.com>
2023-09-12 19:31:25 -0300

    progredindo

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+
+  photoCover: string = "https://pirapop.com.br/wp-content/uploads/2022/10/20221029-quem-ama-cuida-ilustracao.png";
+  contentTitle: string = "Notícias personalizadas";
+  contentDescricao: string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident voluptates cupiditate deserunt vel. Quibusdam mollitia dignissimos enim quis facere impedit! Id eligendi quam praesentium sint impedit delectus molestias deserunt dicta?";
+  contentReturn: string = "VOLTAR";
 
   constructor() { }
 
