0dad4df71e28df5d482d36eee6d54acd2f7ae3b7
SrtaKennedy <ruthellen1800@gmail.com>
2023-09-12 17:08:48 -0300

    navegando entre as paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuSubtitleComponent } from './components/menu-subtitle/menu-subtitle.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MenuSubtitleComponent } from './components/menu-subtitle/menu-subtitle.
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    MenuSubtitleComponent
+    MenuSubtitleComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
